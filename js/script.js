5403229cf7654e4bf2f596d8058bd217ea6e7719
anilraj-rgb <anil.raj9727@gmail.com>
2025-03-19 15:26:51 +0100

    [UPD] create the control for the numbers

--- js/script.js
+++ js/script.js
@@ -13,3 +13,25 @@
 
 // Creo una lista
 const list = document.createElement("ul");
+
+// Utilizzo un ciclo for per controllare il numero e riempire la lista
+for (let i = 1; i <= 100; i++) {
+    const listItem = document.createElement("li");
+    // Controllo se il numero è un multiplo di 3 e di 5
+    if ((i % 3 === 0) && (i % 5 === 0)) {
+        console.log("FizzBuzz");
+        listItem.append("FizzBuzz");
+    // Controllo se il numero è un multiplo di 3
+    } else if (i % 3 === 0) {
+        console.log("Fizz");
+        listItem.append("Fizz");
+        // Controllo se il numero è un multiplo di 5
+    } else if (i % 5 === 0) {
+        console.log("Buzz");
+        listItem.append("Buzz");
+    } else {
+        console.log(i);
+        listItem.append(i);
+    }
+    list.appendChild(listItem);
+};
