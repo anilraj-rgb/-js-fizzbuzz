4121e87d81158f40ce0d4797890974a380eb6028
anilraj-rgb <anil.raj9727@gmail.com>
2025-03-19 15:24:10 +0100

    [UPD]create the structure

---
+++ js/script.js
@@ -0,0 +1,9 @@
+/*
+1. Stampare in console i numeri da 1 a 100 con alcune eccezioni:
+    - se il numero è un multiplo di 3 stampare "Fizz"
+    - se il numero è un multiplo di 5 stampare "Buzz"
+    - se il numero è un multiplo di 3 e di 5 stampare "FizzBuzz"
+2. Creare una lista
+3. Utilizzare il ciclo per riempire la lista
+*/
+   
